e4350f9da2f32fc88c91b9c94fede35d0e913c00
likunlun <mf1823026@smail.nju.edu.cn>
2020-01-13 16:23:19 +0800

    修改OpenBLAS库的路径

--- examples/hg_test/run.sh
+++ examples/hg_test/run.sh
@@ -2,7 +2,7 @@ g++ hg_test.cpp \
 -I ../../build/install/include \
 -L ../../build/install/lib \
 -lnn-static -std=c++11 -fopenmp -O3 \
-~/open_source_projects/OpenBLAS-openmp-install/lib/libopenblas.a
+~/open_source_projects/openblas-install/lib/libopenblas.a
 
 export OMP_NUM_THREADS=4
 ./a.out ../hourglass/data/hg.txt ../hourglass/data/hg.array input.array output.array
